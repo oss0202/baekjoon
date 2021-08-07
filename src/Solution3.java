b4434e3eba3429a4f1e31ffd25ccba8c65a1c984
오성식 <oss0202@naver.com>
2021-08-07 20:50:23 +0900

    백준-1260번 DFS,BFS

---
+++ src/Solution3.java
@@ -0,0 +1,22 @@
+public class Solution3 {
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    public static int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {1,1,1,1,1};
+        int result = Solution3.solution(ints, 3);
+        System.out.println(result);
+    }
+}
