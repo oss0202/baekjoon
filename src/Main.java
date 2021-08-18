0a6c31ad6503e87ea0337c13fc1ce7d39d1aca8e
Oh <oss0202@naver.com>
2021-08-18 19:09:57 +0900

    백준 10950번 A+B-3 A+B를 여러 번 출력하는 문제

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int dan = Integer.parseInt(br.readLine());
-        for (int i = 1 ; i < 10; i++){
-            System.out.println( dan + " * " + i + " = " + dan * i);
+        int runCnt = Integer.parseInt(br.readLine());
+        int a;
+        int b;
+        int[] resultInts = new int[runCnt];
+        String[] strings;
+        String str;
+        for (int i = 0 ; i < resultInts.length; i++){
+            str = br.readLine();
+            strings = str.split(" ");
+            a = Integer.parseInt(strings[0]);
+            b = Integer.parseInt(strings[1]);
+            resultInts[i] = a + b;
         }
+
+        IntStream intStream = Arrays.stream(resultInts);
+        intStream.forEach(System.out::println);
     }
 }
