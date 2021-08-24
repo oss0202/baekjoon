6b6bd944c09d4c9e9dd5cc4a632c1693304cc75f
오성식 <oss0202@naver.com>
2021-08-24 23:11:40 +0900

    백준 2439번 별 찍기 - 2 첫째 줄부터 N번째 줄까지 차례대로 별을 출력

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int rowNum = Integer.parseInt(br.readLine());
-        for (int i = rowNum ; i > 0; i--){
-            System.out.println(i);
-        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         br.close();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                bw.write(' ');
+            }
+            for (int k = 1; k <= i; k++) {
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
         
     }
 }
