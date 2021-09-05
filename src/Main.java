3a8b793c824a5ef05e81aedc83b69a60f8b14849
Oh <oss0202@naver.com>
2021-09-05 15:33:37 +0900

    백준 10952번 A+B - 5 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] firstInput = br.readLine().split(" ");
-        int N = Integer.parseInt(firstInput[0]);
-        int X = Integer.parseInt(firstInput[1]);
-
-        String[] secondInput = br.readLine().split(" ");
-        Arrays.stream(secondInput).filter(s -> Integer.parseInt(s)< X ).forEach(s -> System.out.print(s + " "));
-
-        br.close();
+        String[] firstInput;
+        int result;
+        while (true){
+            firstInput = br.readLine().split(" ");
+            if(Arrays.stream(firstInput).allMatch(s->Integer.parseInt(s)==0)){
+                break;
+            }
+            result = Arrays.stream(firstInput).map(Integer::parseInt).reduce(0, Integer::sum);
+            System.out.println(result);
+        }
     }
 }
