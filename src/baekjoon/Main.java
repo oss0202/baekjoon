6dfb1fe40e49508774bde54b42b83448599e9a68
오성식 <oss0202@naver.com>
2021-10-17 20:40:00 +0900

    https://www.acmicpc.net/problem/10818 최소, 최대 - BufferedReader, 배열 X

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,20 +1,25 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int index = 0;
         int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+        while(st.hasMoreTokens()) {
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
         }
 
-        in.close();
         Arrays.sort(arr);
         System.out.print(arr[0] + " " + arr[N - 1]);
     }
