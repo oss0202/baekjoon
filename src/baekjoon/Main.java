4deb7d3a52dcb1d6149a0213e68f89eb861223ee
오성식 <oss0202@naver.com>
2021-10-24 21:32:23 +0900

    아스키 코드(11654)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,36 +1,12 @@
 package baekjoon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        boolean[] check = new boolean[10001]; // 1부터 10000까지
-
-        //생성자인 수 찾기
-        for (int i = 1; i < check.length; i++) {
-            int n = d(i);
-            if(n < check.length){ // 10000이 넘는 수는 필요가 없음
-                check[n] = true;
-            }
-        }
-
-        //생성자가 없는 수 출력
-        for (int i = 1; i < check.length; i++) {
-            if(!check[i]){
-                System.out.println(i);
-            }
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)br.readLine().charAt(0));
    }
-   // num을 생성자로 하는 수를 리턴하는 함수 생성
-    public static int d(int num){
-        int sum = num;
-        int one = 0;
-        while (num != 0){
-            one = (num % 10); //첫 째 자리수
-            sum += one;
-            num = num/10; // 10으로 나누어 첫 째 자리를 없앤다.
-        }
-        return sum;
-    }
 }
