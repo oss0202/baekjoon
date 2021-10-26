687ddbc6c7146165e0e873c576a403b2e46f44c2
오성식 <oss0202@naver.com>
2021-10-26 21:19:44 +0900

    프로그래머스 - 타겟넘버 - DFS, 재귀

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,31 @@
-import java.io.*;
+import java.io.IOException;
 
 public class Solution {
+    int count = 0;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        br.close();
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N - i; j++) {
-                bw.write(' ');
+        int[] ints = {1,1,1,1,1};
+        int target = 3;
+        Solution solution = new Solution();
+        int result = solution.solution(ints, target);
+        System.out.println(result);
+    }
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        dfs(numbers, 0, target, 0);
+        answer = this.count;
+        return answer;
+    }
+    public void dfs(int[] numbers, int depth, int target, int result){
+        if(depth == numbers.length){
+            if(target == result){
+                this.count++;
             }
-            for (int k = 1; k <= i; k++) {
-                bw.write('*');
-            }
-            bw.newLine();
+            return;
         }
-        bw.flush();
-        bw.close();
-        
+        int add = result + numbers[depth];
+        int sub = result - numbers[depth];
+
+        dfs(numbers, depth + 1, target, add);
+        dfs(numbers, depth + 1, target, sub);
     }
 }
