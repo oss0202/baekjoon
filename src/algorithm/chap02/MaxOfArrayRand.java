47b6605aa0c188f6d2cc0e42253782f3aab6d106
오성식 <oss0202@naver.com>
2021-10-27 22:08:55 +0900

    난수 구하기

---
+++ src/algorithm/chap02/MaxOfArrayRand.java
@@ -0,0 +1,10 @@
+package algorithm.chap02;
+
+import java.util.Random;
+
+public class MaxOfArrayRand {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        rand.nextInt(94);
+    }
+}
