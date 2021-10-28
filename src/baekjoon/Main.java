4835f0d45fbe326c4361893fc1d1bb9dadfb2eac
오성식 <oss0202@naver.com>
2021-10-28 12:50:24 +0900

    벌집(2292)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,34 +3,31 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         /*
-        A : 고정비용
-        B : 가변비용
+        count   벌집  N최솟값
+        1       1
+        2       6     2
+        3       12    8
+        4       18    20
+        5       24    38
 
-        ex. A : 1,000
-            B : 70
-            1대 생산 1070 : 1000 + 70
-            10대 생산 1700 : 1000 + 700
-            C : 가격
+        6의 배수에 따라서 들어가는 방의 수가 증가한다.
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inputNum = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        int BEP = -1;
-        if(C-B > 0){//손익분기점이 없는 경우 제외
-            BEP = A/(C-B) + 1;
+        int count = 1; // 겹 수(최소 루트)
+        int range = 2; // 범위(최솟값 기준)
+        // 6의 배수만큼 빼다가 0보다 작을 때 리턴
+        if(inputNum != 1){
+            while (range <= inputNum){
+                range += ( 6 * count);
+                count++;
+            }
         }
-        
-        // 손익 분기점이 존재않는 경우
-        System.out.println(BEP);
+        System.out.println(count);
    }
 }
