fde77eead0e5f2fc28ae2859084ace6622894082
오성식 <oss0202@naver.com>
2021-10-28 21:31:55 +0900

    벌집(2292) 혼자 해보기

--- src/baekjoon/Main2.java
+++ src/baekjoon/Main2.java
@@ -6,41 +6,24 @@ import java.io.InputStreamReader;
 
 public class Main2 {
     public static void main(String[] args) throws IOException {
-//        String str = "aabbccddaa";
+        /*
+        숫자 범위    방위 수      숫자 갯수
+        1           1              1
+        2 ~ 7       2              6
+        8 ~ 19      3              12
+        20 ~ 37     4              18
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
-        int count = 0;
-        for (int i = 0; i < cnt; i++) {
-            String str = br.readLine();
-            if(check(str)){
-                count++;
+        int N = Integer.parseInt(br.readLine());
+//        int N = 6;
+        int cnt = 1;
+        int multiplyNum = 2;
+        if( N != 1 ){
+            while(multiplyNum <= N){
+                multiplyNum += cnt * 6;
+                cnt++;
             }
         }
-        System.out.println(count);
-    }
-
-    public static boolean check(String str){
-        boolean[] booleans = new boolean[26];
-        int prev = 0;
-        for (int i = 0; i < str.length(); i++) {
-            int now = str.charAt(i);
-            // 1. 이전 단어와 같다면
-            if(now == prev){
-                //      1) 그냥 진행
-                continue;
-                // 2. 이전 단어와 같지 않다면
-            }else{
-                //      1) 중복이 아니라면 배열 변경 및 이전값 세팅
-                int nowIndex = now - 97;
-                if(booleans[nowIndex] != true){
-                    booleans[nowIndex] = true;
-                    prev = now;
-                    //      2) 중복이라면 false
-                }else {
-                    return false;
-                }
-            }
-        }
-        return true;
+        System.out.println(cnt);
     }
 }
