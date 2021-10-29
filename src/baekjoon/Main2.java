58cde3f4e1b1d2edb1e18e846092be39bf4cf71c
오성식 <oss0202@naver.com>
2021-10-30 00:00:32 +0900

    분수찾기(74) 이해하기

--- src/baekjoon/Main2.java
+++ src/baekjoon/Main2.java
@@ -14,6 +14,24 @@ public class Main2 {
          */
 //        Scanner in = new Scanner(System.in);
 //        int X = in.nextInt();
+        /*
+        In : Input 값
+        nowCrossCnt : 현재 분수들 갯수
+        preCrossCnt : 이전 분수들 갯수
+        goal : 나와야 하는 분수
+
+
+        1. 대각선 홀수
+          In    nowCrossCnt     preCrossCnt     goal
+          4     3               3               3/1
+          5     3               3               2/2
+          6     3               3               1/3
+
+          분자 : In - (nowCrossCnt - preCrossCnt - 1)
+          분모 : In - preCrossCnt
+        2. 대각선 짝수
+        반대로
+         */
         int X = 3;
 
         int cross_count = 1, prev_count_sum = 0;
@@ -24,7 +42,7 @@ public class Main2 {
             if (X <= prev_count_sum + cross_count) {
 
                 if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면
-                    // 분모가 큰 수부터 시작
+                    // 분자가 큰 수부터 시작
                     // 분모는 대각선 개수 - (X 번째 - 직전 대각선까지의 누적합 - 1)
                     // 분자는 X 번째 - 직전 대각선까지의 누적합
                     System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
