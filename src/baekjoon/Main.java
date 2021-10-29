31b0e8cbceeb7ce8896e8d31d5af9ab2163d6a63
오성식 <oss0202@naver.com>
2021-10-29 22:11:59 +0900

    분수찾기(74)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -7,27 +7,49 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         /*
-        count   벌집  N최솟값
+        라인     배열 갯수
         1       1
-        2       6     2
-        3       12    8
-        4       18    20
-        5       24    38
-
-        6의 배수에 따라서 들어가는 방의 수가 증가한다.
+        2       2
+        3       3
+        4       4
+        5       5
          */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputNum = Integer.parseInt(br.readLine());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int inputNum = Integer.parseInt(br.readLine());
+        // 입력받는 수
+        int inputNum = 5;
+        // 각 라인별 입력 받을 수 있는 수
+        int lineNum = 1;
+        // 각 라인별 합산
+        int lineSum = 0;
+        // 분자
+        int numerator = 1;
+        // 분모
+        int denominator = 1;
+        // 리턴 결과
+        String result = numerator + "/" + denominator;
 
-        int count = 1; // 겹 수(최소 루트)
-        int range = 2; // 범위(최솟값 기준)
-        // 6의 배수만큼 빼다가 0보다 작을 때 리턴
-        if(inputNum != 1){
-            while (range <= inputNum){
-                range += ( 6 * count);
-                count++;
+        int i = 1;
+        int arrCnt = 1;
+        if(inputNum != 1) {
+            while (arrCnt <= inputNum) {
+                lineNum++;
+
+                //매 처음 시작 세팅(초기화)
+                // 분자 = 1
+                // 분모 = 라인 수
+                numerator = lineNum;
+                denominator = 1;
+                lineSum = i + 2;
+                for (int j = 1; j <= lineNum; j++) {
+                    arrCnt++;
+                    denominator = j;
+                    numerator= lineSum - numerator;
+                    result = numerator + "/" + denominator;
+                }
+                i++;
             }
         }
-        System.out.println(count);
+        System.out.println(result);
    }
 }
--- src/baekjoon/Main2.java
+++ src/baekjoon/Main2.java
@@ -3,27 +3,44 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) throws IOException {
         /*
-        숫자 범위    방위 수      숫자 갯수
-        1           1              1
-        2 ~ 7       2              6
-        8 ~ 19      3              12
-        20 ~ 37     4              18
+        입력받을 X값
+        해당 범위의 대각선 개수 ( cross_count )
+        해당 대각선 직전 대각선 까지의 칸의 누적 합 ( prev_count_sum )
          */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-//        int N = 6;
-        int cnt = 1;
-        int multiplyNum = 2;
-        if( N != 1 ){
-            while(multiplyNum <= N){
-                multiplyNum += cnt * 6;
-                cnt++;
+//        Scanner in = new Scanner(System.in);
+//        int X = in.nextInt();
+        int X = 3;
+
+        int cross_count = 1, prev_count_sum = 0;
+
+        while (true) {
+
+            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+            if (X <= prev_count_sum + cross_count) {
+
+                if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면
+                    // 분모가 큰 수부터 시작
+                    // 분모는 대각선 개수 - (X 번째 - 직전 대각선까지의 누적합 - 1)
+                    // 분자는 X 번째 - 직전 대각선까지의 누적합
+                    System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+                    break;
+                }
+
+                else {	// 대각선의 개수가 짝수라면
+                    // 홀수일 때의 출력을 반대로
+                    System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+                    break;
+                }
+
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
             }
         }
-        System.out.println(cnt);
     }
 }
