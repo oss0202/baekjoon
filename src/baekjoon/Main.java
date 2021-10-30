d8058f786fe2ea7ae1541be379ac8b6630991256
오성식 <oss0202@naver.com>
2021-10-30 20:30:03 +0900

    ACM 호텔(10250)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -7,27 +7,50 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int A = 100;
-//        int B = 99;
-//        int V = 1000000000;
+        /*int H = 30;
+        int W = 50;
+        int N = 72;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
 
-        /*
-        정상에서 미끄러 지지 않는다는 조건이 없다면,
-         V / ( A - B )
-         그러나 미끄러 지므로 A는 B보다 한상 1일 더 많아야 한다.
+        호수구하기
+         앞에 수: N / H 의 나머지
+         뒤에 수 : N / H 의 몫 + 1
+
+         앞 : 4
+         뒤 : 02
+
+         H 30, W 50, N 60
+
+         앞 : 30
+         뒤 : 02
          */
-        int day = (V - B) / (A - B);
 
-        if((V - B) % (A - B) != 0){
-            day++;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+
+        int H = 0;
+        int W = 0;
+        int N = 0;
+
+        int hun = 0;
+        int one = 0;
+
+        StringTokenizer st;
+        for (int i = 0; i < cnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+
+            if ( N % H == 0){
+                hun = H;
+                one = N / H;
+            }else{
+                hun = N % H;
+                one = (N/H)+1;
+            }
+
+            System.out.println(hun + String.format("%02d", one));
         }
-
-        System.out.println(day);
    }
 }
