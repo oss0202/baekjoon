1a9922ec31d74eccebd2bd79cd947a504607c47f
오성식 <oss0202@naver.com>
2021-10-30 19:34:18 +0900

    달팽이는 올라가고 싶다(2869)

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,53 +3,31 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int A = 100;
+//        int B = 99;
+//        int V = 1000000000;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
         /*
-        라인     배열 갯수
-        1       1
-        2       2
-        3       3
-        4       4
-        5       5
+        정상에서 미끄러 지지 않는다는 조건이 없다면,
+         V / ( A - B )
+         그러나 미끄러 지므로 A는 B보다 한상 1일 더 많아야 한다.
          */
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int inputNum = Integer.parseInt(br.readLine());
-        // 입력받는 수
-        int inputNum = 5;
-        // 각 라인별 입력 받을 수 있는 수
-        int lineNum = 1;
-        // 각 라인별 합산
-        int lineSum = 0;
-        // 분자
-        int numerator = 1;
-        // 분모
-        int denominator = 1;
-        // 리턴 결과
-        String result = numerator + "/" + denominator;
+        int day = (V - B) / (A - B);
 
-        int i = 1;
-        int arrCnt = 1;
-        if(inputNum != 1) {
-            while (arrCnt <= inputNum) {
-                lineNum++;
-
-                //매 처음 시작 세팅(초기화)
-                // 분자 = 1
-                // 분모 = 라인 수
-                numerator = lineNum;
-                denominator = 1;
-                lineSum = i + 2;
-                for (int j = 1; j <= lineNum; j++) {
-                    arrCnt++;
-                    denominator = j;
-                    numerator= lineSum - numerator;
-                    result = numerator + "/" + denominator;
-                }
-                i++;
-            }
+        if((V - B) % (A - B) != 0){
+            day++;
         }
-        System.out.println(result);
+
+        System.out.println(day);
    }
 }
