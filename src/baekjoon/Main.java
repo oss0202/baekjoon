56d3824a774ef6258d3fee9584e2e949d97e9b04
오성식 <oss0202@naver.com>
2021-11-02 07:59:55 +0900

    아파트 호수

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -7,50 +7,18 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*int H = 30;
-        int W = 50;
-        int N = 72;
+        /*
+        1층 3호 -> 0층 1,2,3호까지의 사람 수 -> 1 + 2 + 3
+        2층 3호 -> 1층 1,2,3호까지의 사람 수 -> 1 + 3 + 6
 
-
-        호수구하기
-         앞에 수: N / H 의 나머지
-         뒤에 수 : N / H 의 몫 + 1
-
-         앞 : 4
-         뒤 : 02
-
-         H 30, W 50, N 60
-
-         앞 : 30
-         뒤 : 02
          */
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
+        int[][] floors = new int[15][15];
+        // 초기값 세팅 -> 0층 i호, i층 1호
+        for (int i = 0; i < floors.length; i++) {
+            floors[0][i] = i;
 
-        int H = 0;
-        int W = 0;
-        int N = 0;
-
-        int hun = 0;
-        int one = 0;
-
-        StringTokenizer st;
-        for (int i = 0; i < cnt; i++) {
-            st = new StringTokenizer(br.readLine());
-            H = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-
-            if ( N % H == 0){
-                hun = H;
-                one = N / H;
-            }else{
-                hun = N % H;
-                one = (N/H)+1;
-            }
-
-            System.out.println(hun + String.format("%02d", one));
         }
+
    }
 }
