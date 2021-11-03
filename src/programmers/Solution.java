5c4450ad9565fdaaccaed421221171ff252ddc7f
오성식 <oss0202@naver.com>
2021-11-03 13:02:46 +0900

    프로그래머스 - 없는 숫자 더하기

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,34 +1,21 @@
 package programmers;
 
+import jdk.internal.org.objectweb.asm.tree.InnerClassNode;
+
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
-        int result = solution(nums);
-
+        int[] numbers = {1,2,3,4,6,7,8,0};
+//        int[] numbers = {5,8,4,0,6,7,9};
+        Solution solution = new Solution();
+        int result = solution.solution(numbers);
         System.out.println(result);
     }
-    public static int solution(int[] nums) {
-        int cnt = 0;
-        // 세수를 더했을 때 소수
-        // 소수 = 본인보다 작은 수로 나누어 지지 않는 수
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int num = nums[i] + nums[j] + nums[k];
-                    if(isPrime(num)){
-                        cnt++;
-                    }
-                }
-            }
-        }
-        return cnt;
-    }
-    static boolean isPrime(int num){
-        for (int i = 2; i < num; i++) {
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return num>1;
+
+    public int solution(int[] numbers) {
+        int sum = 9*(1+9)/2;
+        int result = Arrays.stream(numbers).sum();
+        return sum - result;
     }
 }
