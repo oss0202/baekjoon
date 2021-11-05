457a183367fab7012766766696379d0e5cbba67d
오성식 <oss0202@naver.com>
2021-11-05 10:40:19 +0900

    프로그래머스 - 신규 아이디 추천

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -4,42 +4,65 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-//        String[] participant = {"leo", "kiki", "eden"};
-//        String[] completion = {"eden", "kiki"};
+        String str = "...!@BaT#*..y.abcdefghijklm";
+//        String str = "abcdefghijklmn.p";
+        str = str.toLowerCase();
 
-        String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"stanko", "ana", "mislav"};
+        str = str.replaceAll("[^a-z0-9-_.]","");
 
-        String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String player : participant) {
-            map.put(player, map.getOrDefault(player, 0) + 1);
-        }
-        for (String player : completion) {
-            map.put(player, map.get(player) - 1);
-        }
+        str = str.replaceAll("[.]{2,}",".");
 
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<String, Integer> entry = iterator.next();
-            System.out.println(entry);
-            System.out.println("Key - " + entry.getKey() + ", value - " + entry.getValue());
+        str = str.replaceAll("^[.]|[.]$","");
+
+        if(str.equals("")){
+            str += "a";
         }
 
+        if(str.length() >= 16){
+            str = str.substring(0, 15);
+            str = str.replaceAll("^[.]|[.]$","");
+        }
 
-//        System.out.println(result);
-//        System.out.println(solution(participant, completion));
-    }
-
-    public static String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        int i;
-        for (i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i])){
-                break;
+        if(str.length() <= 2){
+            while (str.length() < 3){
+                str += str.charAt(str.length()-1);
             }
         }
-        return participant[i];
+
+        System.out.println(str);
+    }
+
+    public String solution(String new_id) {
+        //1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        new_id = new_id.toLowerCase();
+
+        //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+
+        //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+        new_id = new_id.replaceAll("[.]{2,}","");
+
+        //4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        new_id = new_id.replaceAll("^[.]|[.]$","");
+
+        //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        if(new_id.equals("")){
+            new_id += "a";
+        }
+
+        //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+        //     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+        if(new_id.length() >= 16){
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("^[.]|[.]$","");
+        }
+
+        //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+        if(new_id.length() <= 2){
+            while (new_id.length() < 3){
+                new_id += new_id.charAt(new_id.length()-1);
+            }
+        }
+        return new_id;
     }
 }
--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -4,24 +4,7 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("첫번째", 1);
-        map.put("두번째", 2);
-        map.put("세번째", 3);
-        map.put("네번째", 4);
-        map.put("다섯번째", 5);
-        map.put("다섯번째", 5);
-
-        Set<Map.Entry<String, Integer>> strings = map.entrySet();
-        strings.stream().forEach(stringIntegerEntry -> {
-            System.out.println(stringIntegerEntry.getKey());
-            System.out.println(stringIntegerEntry.getValue());
-        });
-
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<String, Integer> key = iterator.next();
-            System.out.println(key);
-        }
+        String str = "...!@BaT#*..y.abcdefghijklm";
+        System.out.println(str.substring(0,15));
     }
 }
