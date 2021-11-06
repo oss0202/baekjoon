2b373c7f2c8bd87239ed8cc2e572e9921ca92517
오성식 <oss0202@naver.com>
2021-11-06 21:24:51 +0900

    프로그래머스 - 완주하지 못한 선수(효율성 성공 - HashMap)

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,19 +1,57 @@
 package programmers;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Solution {
     public static void main(String[] args) {
-//        int[] a = {1,2,3,4};
-//        int[] b = {-3,-1,0,2};
-        int[] a = {-1,0,1};
-        int[] b = {1,0,-1};
-        System.out.println(solution(a, b));
+//        String[] participant = {"leo", "kiki", "eden"};
+//        String[] completion = {"eden", "kiki"};
 
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+
+        String result = "";
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, Integer> entry = iterator.next();
+            if(entry.getValue() != 0){
+                result = entry.getKey();
+            }
+        }
+        System.out.println(result);
     }
 
-    public static int solution(int[] a, int[] b) {
-        int result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
+    public static String solution(String[] participant, String[] completion) {
+        String result = "";
+
+        Map<String, Integer> map = new HashMap<>();
+        // 참가자 인원 수(동명이인 포함)
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        // 완주자 제거
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        // 미완주자 추출
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, Integer> entry = iterator.next();
+            if(entry.getValue() != 0){
+                result = entry.getKey();
+            }
         }
         return result;
     }
--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -8,11 +8,10 @@ public class Test {
         integers.add(1);
         integers.add(2);
         integers.add(3);
+        integers.add(2);
 
 
 //        System.out.println(integers.size());
 //        System.out.println(integers.get(integers.size()-1));
-        integers.remove(integers.size()-1);
-        integers.stream().forEach(System.out::println);
     }
 }
