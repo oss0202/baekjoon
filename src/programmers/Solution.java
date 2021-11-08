0189a8e8fe260f59a7df02ed84a91f28dc6f0e8c
오성식 <oss0202@naver.com>
2021-11-08 09:04:36 +0900

    프로그래머스 - 약수의 개수와 덧셈

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,25 +1,37 @@
 package programmers;
 
-import java.util.*;
-
 public class Solution {
     public static void main(String[] args) {
-//        int[] numbers = {2,1,3,4,1};
-        int[] numbers = {5,0,2,7};
-        int[] result = solution(numbers);
-        System.out.println(Arrays.toString(result));
+        //약수 출력하기
+        int num = 14;
+        // left ~ right 배열 만들기
+        int left = 24;
+        int right = 27;
+
+        int result = solution(left, right);
+        System.out.println(result);
     }
 
-    public static int[] solution(int[] numbers) {
-        //중복을 제거하기 위해서 HashSet 자료구조 사용
-        Set<Integer> integerSet = new HashSet<>();
-
-        //2수를 선택해야한다.
-        for (int i = 0; i < numbers.length-1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                integerSet.add(numbers[i] + numbers[j]);
+    public static int solution(int left, int right) {
+        //약수의 갯수
+        int measure;
+        //최종 리턴값
+        int reuslt = 0;
+        for (int i = left; i <= right; i++) {
+            measure = 0;
+            for (int j = 1; j <= i; j++) {
+                if(i%j == 0){
+                    measure++;
+                }
+            }
+            //짝수 : 더하기
+            //홀수 : 빼기
+            if(measure%2 == 0){
+                reuslt+=i;
+            }else{
+                reuslt-=i;
             }
         }
-        return integerSet.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return reuslt;
     }
 }
