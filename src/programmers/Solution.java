38d2c82d611b2fc720f5f9cacd96441e2c2edf1b
오성식 <oss0202@naver.com>
2021-11-09 08:17:23 +0900

    프로그래머스 - 서울에서 김서방 찾기

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -2,36 +2,20 @@ package programmers;
 
 public class Solution {
     public static void main(String[] args) {
-        //약수 출력하기
-        int num = 14;
-        // left ~ right 배열 만들기
-        int left = 24;
-        int right = 27;
-
-        int result = solution(left, right);
-        System.out.println(result);
-    }
-
-    public static int solution(int left, int right) {
-        //약수의 갯수
-        int measure;
-        //최종 리턴값
-        int reuslt = 0;
-        for (int i = left; i <= right; i++) {
-            measure = 0;
-            for (int j = 1; j <= i; j++) {
-                if(i%j == 0){
-                    measure++;
-                }
-            }
-            //짝수 : 더하기
-            //홀수 : 빼기
-            if(measure%2 == 0){
-                reuslt+=i;
-            }else{
-                reuslt-=i;
-            }
+        String[] seoul = {"Jane", "Kim"};
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")){{
+                System.out.println(i);
+            }}
         }
-        return reuslt;
+    }
+    public static String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")){{
+                answer = "김서방은 " + i + "에 있다";
+            }}
+        }
+        return answer;
     }
 }
