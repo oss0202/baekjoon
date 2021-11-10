e064ac8aaf2e0b0bbe8f38f647d045d8c9824001
오성식 <oss0202@naver.com>
2021-11-11 08:26:35 +0900

    프로그래머스 - 나머지가 1이 되는 수 찾기

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,34 +1,18 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = {1,1,3,3,0,1,1};
-        System.out.println(Arrays.toString(solution(arr)));
+        int n = 12;
+
     }
-
-    public static int[] solution(int []arr) {
-        List<Integer> result = new ArrayList();
-        //배열 for문
-        for (int i = 0; i < arr.length; i++) {
-            //첫번째는 무조건 추가
-            if(i==0){
-                result.add(arr[i]);
-
-                //이전 배열과 다르면 추가, 같으면 생략
-            }else{
-                if(arr[i] != arr[i-1]){
-                    result.add(arr[i]);
-                }
+    public int solution(int n) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if( n % i == 1){
+                result = i;
+                break;
             }
         }
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = result.get(i);
-        }
-        return answer;
+        return result;
     }
 }
