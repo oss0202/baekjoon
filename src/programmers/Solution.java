ac0da15a5ed178d032bc878b521a96efa2935cc3
오성식 <oss0202@naver.com>
2021-11-11 09:37:02 +0900

    프로그래머스 - 폰켓몬

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,17 +1,31 @@
 package programmers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public static void main(String[] args) {
-        int n = 12;
+//        int[] nums = {3,1,2,3};
+//        int[] nums = {3,3,3,2,2,4};
+        int[] nums = {1,2,3,4,5,6};
 
     }
-    public int solution(int n) {
+
+    public static int solution(int[] nums) {
+        // 중복을 제거하고 전체/2 개수를 구할 수 있는 지 확인
+        // Set으로 중복 제거
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
         int result = 0;
-        for (int i = 1; i < n; i++) {
-            if( n % i == 1){
-                result = i;
-                break;
-            }
+        // Set의 길이가 전체/2보다 크다면 전체/2를
+        // n * n-1 * n-2
+        if(nums.length/2 < set.size()){
+            result = nums.length/2;
+        // Set의 길이가 전체/2보다 작다면 그데로 출력
+        }else{
+            result = set.size();
         }
         return result;
     }
