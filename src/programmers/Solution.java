e0dd59c7374e1023882dcf4feef353c765074289
오성식 <oss0202@naver.com>
2021-11-12 21:49:01 +0900

    프로그래머스 - 가운데 글자 가져오기

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -5,28 +5,26 @@ import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-//        int[] nums = {3,1,2,3};
-//        int[] nums = {3,3,3,2,2,4};
-        int[] nums = {1,2,3,4,5,6};
+        String s = "abcde";
+        String answer = "";
+        //짝수와 홀수인 경우로 나누어서 정리한다! 짝수면 2글자가 나오고 홀수면 1글자가 나올 것이다.
+        if(s.length()%2==0) {
+            answer = s.substring(s.length()/2-1, s.length()/2+1);
+        } else {
+            answer = s.substring(s.length()/2,s.length()/2+1);
+        }
+
 
     }
 
-    public static int solution(int[] nums) {
-        // 중복을 제거하고 전체/2 개수를 구할 수 있는 지 확인
-        // Set으로 중복 제거
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+    public String solution(String s) {
+        String answer = "";
+        //짝수와 홀수인 경우로 나누어서 정리한다! 짝수면 2글자가 나오고 홀수면 1글자가 나올 것이다.
+        if(s.length()%2==0) {
+            answer = s.substring(s.length()/2-1, s.length()/2+1);
+        } else {
+            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
         }
-        int result = 0;
-        // Set의 길이가 전체/2보다 크다면 전체/2를
-        // n * n-1 * n-2
-        if(nums.length/2 < set.size()){
-            result = nums.length/2;
-        // Set의 길이가 전체/2보다 작다면 그데로 출력
-        }else{
-            result = set.size();
-        }
-        return result;
+        return answer;
     }
 }
