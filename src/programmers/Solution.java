92c7d606467c71d55bfe4cf8f16ff487821d4da1
오성식 <oss0202@naver.com>
2021-11-15 09:07:55 +0900

    프로그래머스 - 예산

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,30 +1,31 @@
 package programmers;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "abcde";
-        String answer = "";
-        //짝수와 홀수인 경우로 나누어서 정리한다! 짝수면 2글자가 나오고 홀수면 1글자가 나올 것이다.
-        if(s.length()%2==0) {
-            answer = s.substring(s.length()/2-1, s.length()/2+1);
-        } else {
-            answer = s.substring(s.length()/2,s.length()/2+1);
-        }
+//        int[] d = {1,3,2,5,4};
+//        int budget = 9;
 
+        int[] d = {2,2,3,3};
+        int budget = 10;
+        int result = solution(d, budget);
+        System.out.println(result);
 
     }
 
-    public String solution(String s) {
-        String answer = "";
-        //짝수와 홀수인 경우로 나누어서 정리한다! 짝수면 2글자가 나오고 홀수면 1글자가 나올 것이다.
-        if(s.length()%2==0) {
-            answer = s.substring(s.length()/2-1, s.length()/2+1);
-        } else {
-            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+    public static int solution(int[] d, int budget) {
+        int supportCnt = 0;
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if(budget - d[i] >= 0){
+                budget = budget- d[i];
+                supportCnt++;
+            }else{
+                break;
+            }
         }
-        return answer;
+        return supportCnt;
     }
 }
