a0ff098fda9bdd4c2dbb94dc58f10870845f1b32
오성식 <oss0202@naver.com>
2021-11-18 21:00:00 +0900

    최소직사각형

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -4,33 +4,17 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int n = 5;
-        int[] arr1 = {9, 20, 28, 18, 11};
-        int[] arr2 = {30, 1, 21, 17, 28};
-        String[] result = solution(n, arr1, arr2);
-        System.out.println(Arrays.toString(result));
+        int[][] result = {{60,50},{30,70},{60,30},{80,40}};
+
+        int maxVal = 0;
+        int minVal = 0;
+        for (int i = 0; i < result.length; i++) {
+        }
+        System.out.println(maxVal);
+        System.out.println(minVal);
     }
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
-        int[] map = new int[n];
-        String[] result = new String[n];
-
-        // 1. 지도1(arr1), 지도2(arr2) or 연산
-        for (int i = 0; i < map.length; i++) {
-            map[i] = arr1[i]|arr2[i];
-        }
-
-        // 2. 자리수가 꽉 차지 않는다면 앞에 0 붙여주기
-        for (int i = 0; i < map.length; i++) {
-            String binaryResult = Integer.toBinaryString(map[i]);
-            int cha = n - binaryResult.length();
-            for (int j = 0; j < cha; j++) {
-                binaryResult = "0" + binaryResult;
-            }
-            // 3. 1은 #으로, 0은 공백으로 치환
-            binaryResult = binaryResult.replaceAll("1","#");
-            binaryResult = binaryResult.replaceAll("0"," ");
-            result[i] = binaryResult;
-        }
-        return result;
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        return answer;
     }
 }
