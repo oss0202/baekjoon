26f152d594b76f43c14f6f798507dfa1d132d01e
오성식 <oss0202@naver.com>
2021-11-20 23:02:49 +0900

    프로그래머스 - 최소직사각형

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,29 +1,31 @@
 package programmers;
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
         int[][] sizes = {{60,50},{30,70},{60,30},{80,40}};
-
         /**
          * 명합지갑 만들기
          *
          * 1. 가로 길이가 세로 길이보다 길도록 배열 값 변경
          * 2. 가로, 세로 max값 구하기
          */
+        int result = solution(sizes);
+        System.out.println(result);
 
-        int temp = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            for (int j = 0; j < sizes.length; j++) {
-                if(sizes[i][0] < sizes[i][1]){
-
-                }
-            }
-        }
     }
-    public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
+    public static int solution(int[][] sizes) {
+        int temp = 0;
+        int maxH = 0;
+        int maxW = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]){
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            maxH = Math.max(maxH, sizes[i][0]);
+            maxW = Math.max(maxW, sizes[i][1]);
+        }
+        return maxW * maxH;
     }
 }
