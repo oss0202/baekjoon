091c488aaa820d8d07e73ebd5bf6c77a8a4e8790
오성식 <oss0202@naver.com>
2021-11-24 21:14:37 +0900

    프로그래머스 - 소수(작업중)

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,45 +1,24 @@
 package programmers;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
-    public static void main(String[] args) {
-        String[] phone_book = {"119", "97674223", "195524421"};
-        boolean reuslt = true;
+    Set<Integer> numberSet = new HashSet<>();
+    public void recursive(String comb, String others){
+        // 1. 현재 조합(comb)을 set에 추가한다.
+        numberSet.add(Integer.valueOf(comb));
+        // 2. 남은 숫자 중 한개를 더 해 새로운 조합을 만든다.
 
-        // 1. HashMap을 만든다.
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < phone_book.length; i++){
-            map.put(phone_book[i], 1);
-        }
-        // 2. 모든 전화번호의 접두어가 HashMap에 있는지 확인한다.
-        out:
-        for(int i = 0; i< phone_book.length; i++){
-            for(int j = 1; j < phone_book[i].length(); j++){
-                if(map.containsKey(phone_book[i].substring(0,j))){
-                    reuslt = false;
-                    break out;
-                }
-            }
-        }
-
-        // 3. 여기까지 왔다면 접두어가 없다는 것이다.
-        System.out.println(reuslt);
     }
 
-    public boolean solution(String[] phone_book) {
-        // 1. HashMap을 선언한다.
-        Map<String, Integer> map = new HashMap<>();
-        // 2. 모든 전화번호를 HashMap에 넣는다.
-        for (int i = 0; i < phone_book.length; i++)
-            map.put(phone_book[i], i);
-        // 3. 모든 전화번호의 substring이 HashMap에 존재하는지 확인한다.
-        for (int i = 0; i < phone_book.length; i++)
-            for (int j = 0; j < phone_book[i].length(); j++)
-                if (map.containsKey(phone_book[i].substring(0, j)))
-                    return false;
-        return true;
+    public int solution(String numbers){
+        // 1. 모든 숫자조합을 모두 만들어 준다.(재귀함수)
+        recursive("", numbers);
+        // 2. Set의 개념을 활용하여 중복되는 조합을 제거
+        // 3. 소수인지 판단(에라토스채 - 루트값을 확인)
+        return 123;
     }
-
 }
