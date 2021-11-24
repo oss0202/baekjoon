ac8563fcd25a81a5a542aa4d2f751303c753b3de
오성식 <oss0202@naver.com>
2021-11-24 09:37:48 +0900

    프로그래머스 - 전화번호 목록(hash)

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -5,12 +5,27 @@ import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
-        String[] phone_book = {"119", "97674223", "1195524421"};
+        String[] phone_book = {"119", "97674223", "195524421"};
+        boolean reuslt = true;
 
-        String str = "abcdefg";
-        System.out.println(str.startsWith("abc"));
-        System.out.println(str.startsWith("abd"));
-        System.out.println(str.startsWith("bcd"));
+        // 1. HashMap을 만든다.
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i], 1);
+        }
+        // 2. 모든 전화번호의 접두어가 HashMap에 있는지 확인한다.
+        out:
+        for(int i = 0; i< phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    reuslt = false;
+                    break out;
+                }
+            }
+        }
+
+        // 3. 여기까지 왔다면 접두어가 없다는 것이다.
+        System.out.println(reuslt);
     }
 
     public boolean solution(String[] phone_book) {
