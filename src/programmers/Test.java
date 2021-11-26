bb7e9fdcc02af5d290070bbe1f649ab595e5b330
오성식 <oss0202@naver.com>
2021-11-26 11:21:27 +0900

    프로그래머스 - 테스트

--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -1,8 +1,23 @@
 package programmers;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test {
     public static void main(String[] args) {
         String str = "abcdefg";
         System.out.println(str.substring(1,3));
+    }
+
+    public int solution(int[] A) {
+        Set<Integer> integerSet = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            integerSet.add(A[i]);
+        }
+
+
+
+        return 0;
     }
 }
