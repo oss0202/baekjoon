8edffb4145a4142ebcdede6eb0805ce3f46db28b
오성식 <oss0202@naver.com>
2021-11-29 07:50:07 +0900

    java substring 한글자씩

--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -7,7 +7,13 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         String str = "abcdefg";
-        System.out.println(str.substring(1,3));
+//        System.out.println(str.substring(1,3));
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(0,i) + " " + str.substring(i+1));
+        }
+
+//        System.out.println(str.substring(0,3));
     }
 
     public int solution(int[] A) {
