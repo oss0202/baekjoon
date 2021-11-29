8b79796fc9be2ed0211296bae7e1754cc792e625
오성식 <oss0202@naver.com>
2021-11-29 11:05:26 +0900

    Wrapper class test

--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -12,6 +12,8 @@ public class Test {
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.substring(0,i) + " " + str.substring(i+1));
         }
+//        int result = Integer.parseInt(null);
+        Integer result2 = null;
 
 //        System.out.println(str.substring(0,3));
     }
