0f91002b2f605603ef43176ef0694c04b91bd660
오성식 <oss0202@naver.com>
2021-12-02 17:02:21 +0900

    ArrayDequeTest 테스트

---
+++ src/algorithm/ArrayDequeTest.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import javax.xml.soap.Node;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayDequeTest {
+    public static void main(String[] args) {
+        Deque<Node> nodes = new LinkedList<>();
+        List<Node> list = new LinkedList<>();
+    }
+}
