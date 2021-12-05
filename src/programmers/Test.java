fc5e2fa80ddb48e3fa4676b75b1ee4a0d9b52988
오성식 <oss0202@naver.com>
2021-12-06 08:48:41 +0900

    String 문자열 규칙적으로 짜르기 테스트

--- src/programmers/Test.java
+++ src/programmers/Test.java
@@ -4,7 +4,6 @@ public class Test {
     public static void main(String[] args) {
         String str = "abcdefg";
         for (int i = 1; i < str.length()/2; i++) {
-            System.out.println(i);
             for (int j = 0; j < str.length()/i; j++) {
                 System.out.println(str.substring(j*i, (j*i)+i));
             }
