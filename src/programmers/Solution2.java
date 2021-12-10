c95b0471d20c93636101bae76c785facdfbf3ad0
오성식 <oss0202@naver.com>
2021-12-10 22:43:03 +0900

    프로그래머스 - N개의 최소공배수

--- src/programmers/Solution2.java
+++ src/programmers/Solution2.java
@@ -1,59 +1,32 @@
 package programmers;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
 public class Solution2 {
-    public static void main(String[] args) {
-        Solution2 solution2 = new Solution2();
-        System.out.println(solution2.solution("17"));
+    // 5. 최대공약수 메소드, 유클리드 호제법
+    public static int gcd(int a, int b){
+        int x = Math.max(a,b);
+        int y = Math.min(a,b);
+
+        while(x % y != 0){
+            int r = x % y;
+            x = y;
+            y = r;
+        }
+
+        return y;
     }
 
-    public int solution(String numbers) {
-        int count = 0;
-        // 1. 재귀를 이용해서 모든 소수를 구한다.
-        recursive("", numbers);
-        System.out.println(set);
+    public int solution(int[] arr) {
+        // 1. 배열의 첫 번째 값을 저장.
+        int answer = arr[0];
 
-        // 2.소수인지를 판단한다.
-        Iterator<Integer> it = set.iterator();
-        while (it.hasNext()){
-            if(isPrime(it.next())){
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private boolean isPrime(Integer next) {
-        // 1. 0과 1일 경우에는 false
-        if( next == 0 || next == 1){
-            return false;
-        }
-        // 2. 에라토스테에스의 체를 이용해서 소수인지 확인하기
-        int lim = (int) Math.sqrt(next);
-        for (int i = 1; i < lim; i++) {
-            if(next % lim == 0){
-                return false;
-            }
-        }
-        // 3. 나머지 경우에는 소수이다.
-        return true;
-    }
-
-    Set<Integer> set = new HashSet<>();
-    private void recursive(String comb, String others) {
-        // 1. 처음 호출했을 경우를 제외하고 소수Set에 추가해준다.
-        // -> 첫 호출은 comb가 비어있기 때문에
-        if(!comb.equals("")){
-            set.add(Integer.valueOf(comb));
+        // 2. 배열의 길이가 1이면 반복x, 그 이상은 다음 인덱스인 1부터 반복.
+        for(int i = 1;i<arr.length;i++){
+            // 3. 두 값의 최대공약수
+            int gcd = gcd(answer,arr[i]);
+            // 4. 두 값의 최소공배수
+            answer = answer * arr[i] / gcd;
         }
 
-        // 2. 남은 숫자들을 이용해서 새로운 조합을 만든다.
-        for(int i = 0; i < others.length(); i++){
-            String inputOthers = others.substring(0, i) + others.substring(i+1);
-            recursive(comb + others.charAt(i), inputOthers);
-        }
+        return answer;
     }
 }
