d318654ecfb8d25ff2411acfb558c246fadac5b3
오성식 <oss0202@naver.com>
2021-12-14 14:09:33 +0900

    프로그래머스 - 최소직사각형

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -3,28 +3,19 @@ package programmers;
 import java.util.Arrays;
 
 public class Solution {
-    public int solution(int[] nums) {
-        int cnt = 0;
-        // 세수를 더했을 때 소수
-        // 소수 = 본인보다 작은 수로 나누어 지지 않는 수
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int num = nums[i] + nums[j] + nums[k];
-                    if(isPrime(num)){
-                        cnt++;
-                    }
-                }
+    public int solution(int[][] sizes) {
+        int temp = 0;
+        int maxH = 0;
+        int maxW = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]){
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
             }
+            maxH = Math.max(maxH, sizes[i][0]);
+            maxW = Math.max(maxW, sizes[i][1]);
         }
-        return cnt;
-    }
-    static boolean isPrime(int num){
-        for (int i = 2; i < num; i++) {
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return num>1;
+        return maxW * maxH;
     }
 }
