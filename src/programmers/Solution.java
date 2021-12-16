b2a149b00c6a27f15b28fcbf8ee932defb054855
=오성식 <oss0202@naver.com>
2021-12-16 18:19:19 +0900

    프로그래머스 - 실패율(정답)

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,21 +1,55 @@
 package programmers;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
-    public int solution(int[][] sizes) {
-        int temp = 0;
-        int maxH = 0;
-        int maxW = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            if(sizes[i][0] < sizes[i][1]){
-                temp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = temp;
+    public int[] solution(int N, int[] stages) {
+        // 각 스테이지별 인원 수 구하기(N+1은 마지막 스테이지 클리어한 사람)
+        int[] stagePerson = new int[N];
+        for (int i = 0; i < stages.length; i++) {
+            if(stages[i] != N+1){
+                stagePerson[stages[i]-1]++;
             }
-            maxH = Math.max(maxH, sizes[i][0]);
-            maxW = Math.max(maxW, sizes[i][1]);
         }
-        return maxW * maxH;
+        // 실패율 list 생성
+        List<Double> failRate = new ArrayList<>();
+        // 인덱스계산을 위한 실패율 array 생성
+        double[] failArray = new double[N];
+
+        // 스테이지에 도달한 명 수
+        double num = stages.length;
+        // 다음 스테이지 인원 수
+        double remain = 0;
+
+        //failArray 배열 실패율
+        for (int i = 0; i < stagePerson.length; i++) {
+            remain = stagePerson[i];
+            if(num == 0){
+                failArray[i] = 0;
+            }else{
+                failArray[i] = stagePerson[i]/num;
+                num = num - remain;
+            }
+            failRate.add(failArray[i]);
+        }
+        //내림차순 정렬
+        Collections.sort(failRate, Collections.reverseOrder());
+
+        //정렬된 실패율값과 stage값을 비교해서 같으면 인덱스 번호를 가져온다.
+        int[] result = new int[N];
+
+        //중복된 값이 있을 수 있으므로 꺼낸 인덱스의 값은 -1로 변환해준다.
+        for (int i = 0; i < failRate.size(); i++) {
+            for (int j = 0; j < failArray.length; j++) {
+                if(failRate.get(i) == failArray[j]){
+                    result[i] = j+1;
+                    failArray[j] = -1;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
