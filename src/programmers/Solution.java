67a4c64c232acf1bc0fdddc5cc1a9d8cb9da0bd0
=오성식 <oss0202@naver.com>
2021-12-18 17:39:17 +0900

    프로그래머스 - 약수의 개수와 덧셈(정답)

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -3,18 +3,26 @@ package programmers;
 import java.util.Arrays;
 
 public class Solution {
-    public int solution(int[] d, int budget) {
-        int supportCnt = 0;
-        Arrays.sort(d);
-
-        for (int i = 0; i < d.length; i++) {
-            if(budget - d[i] >= 0){
-                budget = budget- d[i];
-                supportCnt++;
+    public int solution(int left, int right) {
+        //약수의 갯수
+        int measure = 0;
+        //최종 리턴값
+        int reuslt = 0;
+        for (int i = left; i <= right; i++) {
+            measure = 0;
+            for (int j = 1; j <= i; j++) {
+                if(i%j == 0){
+                    measure++;
+                }
+            }
+            //짝수 : 더하기
+            //홀수 : 빼기
+            if(measure%2 == 0){
+                reuslt+=i;
             }else{
-                break;
+                reuslt-=i;
             }
         }
-        return supportCnt;
+        return reuslt;
     }
 }
