b65b06c8f8057441ee90bdca9bf53f2bd559ba6b
오성식 <oss0202@naver.com>
2021-12-25 20:40:38 +0900

    프로그래머스 - 모의고사

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -3,20 +3,43 @@ package programmers;
 import java.util.Arrays;
 
 public class Solution {
-    public boolean solution(String[] phone_book) {
+    static int[] p1 = {1,2,3,4,5};
+    static int[] p2 = {2,1,2,3,2,4,2,5};
+    static int[] p3 = {3,3,1,1,2,2,4,4,5,5};
 
-        boolean result = true;
+    public int[] solution(int[] answers) {
+        int[] score = new int[3];
 
-        Arrays.sort(phone_book);
-
-        out:
-        for (int i = 0; i < phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                result = false;
-                break out;
+        // 사람 별로 점수 계산
+        for (int i = 0; i < answers.length; i++) {
+            if(answers[i] == p1[i%5]) score[0]++;
+            if(answers[i] == p2[i%8]) score[1]++;
+            if(answers[i] == p3[i%10]) score[2]++;
+        }
+        // 가장 높은 점수 구하기
+        int max = 0;
+        for (int i = 0; i < 3; i++) {
+            if(score[i] > max){
+                max = score[i];
             }
         }
 
+        // 높은 점수 카운트
+        int maxCnt = 0;
+        for (int i = 0; i < 3; i++) {
+            if(max == score[i]){
+                maxCnt++;
+            }
+        }
+
+        int[] result = new int[maxCnt];
+        int index = 0;
+        // 최고 높은 점수를 받은 사람 배열 생성
+        for (int i = 0; i < 3; i++) {
+            if(max == score[i]){
+                result[index++] = i + 1;
+            }
+        }
         return result;
     }
 }
