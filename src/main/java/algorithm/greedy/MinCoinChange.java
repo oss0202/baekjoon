d4cb205dece692016cd2ad1f6d9747f6e3287799
오성식 <oss0202@naver.com>
2021-12-27 13:22:57 +0900

    Greedy - 조이스틱

---
+++ src/main/java/algorithm/greedy/MinCoinChange.java
@@ -0,0 +1,17 @@
+package algorithm.greedy;
+
+public class MinCoinChange {
+    public static void main(String[] args) {
+        char result = 'A' + 9;
+//        System.out.println(result++);
+//        System.out.println(result);
+
+        System.out.println("--------");
+        char minus = 'J' - 'A';
+        char minus2 = 'Z' - 'J';
+        char plus = 'J' + 'A';
+        System.out.println(minus);
+        System.out.println(minus2);
+        System.out.println(plus);
+    }
+}
