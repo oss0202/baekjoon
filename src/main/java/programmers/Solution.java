e9e6c8045d523276055e010f68d84f6a997dbfe8
오성식 <oss0202@naver.com>
2021-12-29 19:10:14 +0900

    프로그래머스 - 프린터

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,42 +1,41 @@
 package programmers;
 
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Solution {
-    public static void main(String[] args) {
-//        long result = 1234L;
-//        System.out.println(result + "");
-//
-//        String resultStr = result + "";
-//        for (int i = 0; i < resultStr.length(); i++) {
-//            System.out.println(resultStr.charAt(i));
-//        }
-//
-//        System.out.println("----------------");
-//
-//        int resultInt = (int) result;
-//        while (resultInt > 0){
-//            System.out.println(resultInt%10);
-//            resultInt /= 10;
-//        }
-        System.out.println(Arrays.toString(solution(12345)));
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
 
-    }
-    public static int[] solution(long n) {
-        /**
-         * 1. 뒤에서부터 하나씩 빼오기
-         * 2. while문 돌면서 변경된 사항 넣어주기
-         */
-        int resultInt = (int) n;
-        int[] result = new int[(n+"").length()];
-        int i = 0;
-        while (resultInt > 0){
-            result[i++] = resultInt%10;
-            resultInt /= 10;
+        /*** PriorityQueue(내림차순정렬) */
+        for (int i = 0; i < priorities.length; i++) {
+            priorityQueue.add(priorities[i]);
         }
-        return result;
 
-
+        /**
+         * PriorityQueue(내림차순정렬)를 순회하면서
+         * 인쇄문서(priorities)와 비교하여 index 카운트
+         * */
+        out:
+        while (!priorityQueue.isEmpty()){
+            for (int i = 0; i < priorities.length; i++) {
+                /*** PriorityQueue와 같다면 카운트증가*/
+                if(priorities[i] == priorityQueue.peek()){
+                    /*** 마지막 for문일 경우 그대로 return */
+                    if(i == location){
+                        answer++;
+                        break out;
+                    }
+                    priorityQueue.poll();
+                    answer++;
+                }
+            }
+        }
+        if(priorityQueue.isEmpty()){
+            answer = -1;
+        }
+        return answer;
     }
 }
