4f381c65d12b6ae8b5ddf415fdd2c19982bdb7f1
오성식 <oss0202@naver.com>
2021-12-30 17:00:41 +0900

    프로그래머스 - 예산

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,41 +1,40 @@
 package programmers;
 
 
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 public class Solution {
-    public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        int answer = 0;
+    public static void main(String[] args) {
 
-        /*** PriorityQueue(내림차순정렬) */
-        for (int i = 0; i < priorities.length; i++) {
-            priorityQueue.add(priorities[i]);
-        }
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
 
+        System.out.println(solution(d,budget));
+    }
+    public static int solution(int[] d, int budget) {
         /**
-         * PriorityQueue(내림차순정렬)를 순회하면서
-         * 인쇄문서(priorities)와 비교하여 index 카운트
-         * */
-        out:
-        while (!priorityQueue.isEmpty()){
-            for (int i = 0; i < priorities.length; i++) {
-                /*** PriorityQueue와 같다면 카운트증가*/
-                if(priorities[i] == priorityQueue.peek()){
-                    /*** 마지막 for문일 경우 그대로 return */
-                    if(i == location){
-                        answer++;
-                        break out;
-                    }
-                    priorityQueue.poll();
-                    answer++;
-                }
+         * 최대 몇개의 부서에 예산이 지원 가능한지 확인
+         * 1. 오름차순으로 부서별 예산 정렬
+         * 2. 부서별 예산 리스트 for문
+         *  1) 남은 예산 - 부서예산이 0보다 클 경우
+         *  - 예산 지급
+         *  - 지급 숫자 count
+         *  2) 나머지
+         *  - 더이상 예산을 지급하지 못하므로 종료처리
+         */
+        int restBudget = 0;
+        int budgetCnt = 0;
+
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            restBudget = budget - d[i];
+            if(restBudget >= 0){
+                budget = restBudget;
+                budgetCnt++;
+            }else{
+                break;
             }
         }
-        if(priorityQueue.isEmpty()){
-            answer = -1;
-        }
-        return answer;
+        return budgetCnt;
     }
 }
