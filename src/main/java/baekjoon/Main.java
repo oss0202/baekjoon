345d36169a2de3d48cb3144c85ccc62edadbdd5f
오성식 <oss0202@naver.com>
2022-01-01 18:15:01 +0900

    stream debug test

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -1,24 +1,12 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        /*
-        1층 3호 -> 0층 1,2,3호까지의 사람 수 -> 1 + 2 + 3
-        2층 3호 -> 1층 1,2,3호까지의 사람 수 -> 1 + 3 + 6
-
-         */
-
-        int[][] floors = new int[15][15];
-        // 초기값 세팅 -> 0층 i호, i층 1호
-        for (int i = 0; i < floors.length; i++) {
-            floors[0][i] = i;
-
-        }
-
-   }
+    public static void main(String[] args){
+        int[] result = {1,2,3,4,5,6};
+        Arrays.stream(result)
+                .filter(value -> value>3)
+                .forEach(System.out::println);
+    }
 }
