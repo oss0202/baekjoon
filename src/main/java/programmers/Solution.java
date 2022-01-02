8c17ef9d0a126b6865554f3a344528db96be6008
=오성식 <oss0202@naver.com>
2022-01-02 17:26:21 +0900

    프로그래머스 - 해시 - 완주하지 못한 선수

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,27 +1,42 @@
 package programmers;
 
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
+        String[] participant  = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+    }
+    public String solution(String[] participant, String[] completion) {
         /**
-         * 1. 배열 자르기
-         * 2. 배열 정렬하기
-         * 3. n번째 숫자 뽑아내기
+         * 1. participant를 key로 가지는 map만들기
+         *  - value(default는 0) + 1
+         * 2. completion를 순회하면서
+         *  - participant에 있는 key값을 찾아서 value -1하기
+         * 3. participant를(key, value모두 필요하니 entrySet) 순회하면서 value값이 0이 아닌 key return
          */
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+        String result = null;
+        Map<String, Integer> pMap = new HashMap<>();
 
-        int[] answer = new int[commands.length];
-        int index = 0;
-        for (int i = 0; i < commands.length; i++) {
-            int[] command = commands[i];
-            int[] result = Arrays.copyOfRange(array, command[0]-1, command[1]);
-
-            Arrays.sort(result);
-            answer[index++] = result[command[2]-1];
+        for (int i = 0; i < participant.length; i++) {
+            pMap.put(participant[i], pMap.getOrDefault(participant[i], 0) + 1);
         }
-        System.out.println(Arrays.toString(answer));
+
+        for (int i = 0; i < completion.length; i++) {
+            pMap.put(completion[i], pMap.getOrDefault(completion[i], 0) - 1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> entryIterator = pMap.entrySet().iterator();
+        while (entryIterator.hasNext()){
+            Map.Entry<String, Integer> entry = entryIterator.next();
+            if(entry.getValue() != 0){
+                result = entry.getKey();
+//                System.out.println(entry.getKey());
+            }
+        }
+        return result;
     }
 }
