0dcd1d7dfe7e8cacb1afec429f7215fc60a53f99
오성식 <oss0202@naver.com>
2022-01-03 16:36:56 +0900

    프로그래머스 - 해시 - 위장

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -6,37 +6,28 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) {
-        String[] participant  = {"leo", "kiki", "eden"};
-        String[] completion = {"eden", "kiki"};
-    }
-    public String solution(String[] participant, String[] completion) {
+    public int solution(String[][] clothes) {
         /**
-         * 1. participant를 key로 가지는 map만들기
-         *  - value(default는 0) + 1
-         * 2. completion를 순회하면서
-         *  - participant에 있는 key값을 찾아서 value -1하기
-         * 3. participant를(key, value모두 필요하니 entrySet) 순회하면서 value값이 0이 아닌 key return
+         * 1. 각 의상종류별 count하기
+         *  -  HashMap을 이용
+         *  - String(의상종류), Integer(숫자)
+         * 3. 조합의 수 계산하기
+         *  - HashMap에서 뒤에 숫자들 곱한 다음 -1
+         *  - 모두 안입을 경우 빼주기
+         *  - (n X n X n) -1
          */
-        String result = null;
-        Map<String, Integer> pMap = new HashMap<>();
-
-        for (int i = 0; i < participant.length; i++) {
-            pMap.put(participant[i], pMap.getOrDefault(participant[i], 0) + 1);
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            String[] clothe = clothes[i];
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
-
-        for (int i = 0; i < completion.length; i++) {
-            pMap.put(completion[i], pMap.getOrDefault(completion[i], 0) - 1);
-        }
-
-        Iterator<Map.Entry<String, Integer>> entryIterator = pMap.entrySet().iterator();
+        Iterator<Map.Entry<String, Integer>> entryIterator = map.entrySet().iterator();
         while (entryIterator.hasNext()){
-            Map.Entry<String, Integer> entry = entryIterator.next();
-            if(entry.getValue() != 0){
-                result = entry.getKey();
-//                System.out.println(entry.getKey());
-            }
+            Map.Entry<String, Integer> innerMap = entryIterator.next();
+            answer *= (innerMap.getValue() +1 );
         }
-        return result;
+
+        return answer-1;
     }
 }
