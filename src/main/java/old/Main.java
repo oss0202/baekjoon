fcce6b50cee7d5a8b222af6c1f9d52bfd50de5fb
오성식 <oss0202@naver.com>
2022-01-04 14:54:59 +0900

    java - stream - map

--- src/main/java/old/Main.java
+++ src/main/java/old/Main.java
@@ -1,21 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+package old;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        while (true){
-            st = new StringTokenizer(br.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            if(A==0 && B==0)break;
-            sb.append((A+B)).append('\n');
-        }
-        System.out.println(sb);
+    public static void main(String[] args) {
+        List<String> list =
+                Arrays.asList("a1", "a2", "b1", "b2", "c2", "c1", "c3");
+        Stream<String> stream2 = list.stream();
+        stream2.map(s -> s.toUpperCase()).forEach(System.out::println);
     }
 }
