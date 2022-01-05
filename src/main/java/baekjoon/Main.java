dff25f07e0392668a4565338b61fb45570b09c95
오성식 <oss0202@naver.com>
2022-01-05 21:14:52 +0900

    Stream debug test

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args){
         int[] result = {1,2,3,4,5,6};
         Arrays.stream(result)
+                .map(value -> value + 3)
                 .filter(value -> value>3)
                 .forEach(System.out::println);
     }
