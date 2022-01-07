7b8b644bf277383bd7cc91c9a92805e74ba19e7b
오성식 <oss0202@naver.com>
2022-01-07 20:08:26 +0900

    프로그래머스 - 정렬 - 가장 큰 수

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -2,50 +2,40 @@ package programmers;
 
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
-        /**
-         * 1. 배열 자르기
-         *  - Arrays.copyOfRange( 배열, 시작인덱스, 해당인덱스 앞에서 자름)
-         *  - Arrays.copyOfRange( 배열, command -1, command)
-         * 2. 배열 정렬하기
-         *  - Arrays.sort( 배열 )
-         * 3. 숫자 추출하기
-         *  - 배열[인덱스]
-         *  - 배열[command -1]
-         */
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[] command = {2,5,3};
-
-        int[] result = Arrays.copyOfRange(array, 2-1,5);
-        Arrays.sort(result);
-        System.out.println(Arrays.toString(result));
-
+        int[] numbers = {6, 10, 2};
+        Solution solution = new Solution();
+        System.out.println(solution.solution(numbers));
     }
+    public String solution(int[] numbers) {
+        /**
+         * 1. int 배열을 integer 리스트로 변환
+         * 2. 두개씩 더했을 때 큰수가 되도록 내림차순 정렬
+         * 3. 정렬된 숫자 합치기
+         *  - 첫자리가 0이라면 0
+         */
+        List<Integer> integerList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
 
-    public int[] solution(int[] array, int[][] commands){
-        int[] answer = new int[commands.length];
-        int index = 0;
-        for (int i = 0; i < commands.length; i++) {
-            int[] command = commands[i];
-            /**
-             * 1. 배열 자르기
-             *  - Arrays.copyOfRange( 배열, 시작인덱스, 해당인덱스 앞에서 자름)
-             *  - Arrays.copyOfRange( 배열, command -1, command)
-             */
-            int[] result = Arrays.copyOfRange(array, command[0]-1,command[1]);
-             /** 2. 배열 정렬하기
-             *  - Arrays.sort( 배열 )
-             */
-             Arrays.sort(result);
+        Collections.sort(integerList, (a, b) -> {
+            String as = String.valueOf(a), bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
 
-             /** 3. 숫자 추출하기
-             *  - 배열[인덱스]
-             *  - 배열[command -1]
-             */
-             answer[index++] = result[command[2]-1];
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : integerList) {
+            sb.append(i);
+        }
+
+        String answer = sb.toString();
+        if(answer.charAt(0) == '0'){
+            answer = "0";
         }
         return answer;
     }
+
 }
