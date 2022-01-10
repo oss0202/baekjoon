2f56831f495c5a3764a7a97c506a9b8eff611a14
오성식 <oss0202@naver.com>
2022-01-10 16:40:54 +0900

    프로그래머스 - 스택/큐 - 프린터

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,39 +1,40 @@
 package programmers;
 
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
 
 public class Solution {
-    public static void main(String[] args) {
-        int[] numbers = {6, 10, 2};
-        Solution solution = new Solution();
-        System.out.println(solution.solution(numbers));
-    }
-    public String solution(int[] numbers) {
-        /**
-         * 1. int 배열을 integer 리스트로 변환
-         * 2. 두개씩 더했을 때 큰수가 되도록 내림차순 정렬
-         * 3. 정렬된 숫자 합치기
-         *  - 첫자리가 0이라면 0
-         */
-        List<Integer> integerList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
 
-        Collections.sort(integerList, (a, b) -> {
-            String as = String.valueOf(a), bs = String.valueOf(b);
-            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
-        });
-
-        StringBuilder sb = new StringBuilder();
-        for (Integer i : integerList) {
-            sb.append(i);
+        /*** PriorityQueue(내림차순정렬) */
+        for (int i = 0; i < priorities.length; i++) {
+            priorityQueue.add(priorities[i]);
         }
 
-        String answer = sb.toString();
-        if(answer.charAt(0) == '0'){
-            answer = "0";
+        /**
+         * PriorityQueue(내림차순정렬)를 순회하면서
+         * 인쇄문서(priorities)와 비교하여 index 카운트
+         * */
+        out:
+        while (!priorityQueue.isEmpty()){
+            for (int i = 0; i < priorities.length; i++) {
+                /*** PriorityQueue와 같다면 카운트증가*/
+                if(priorities[i] == priorityQueue.peek()){
+                    /*** 마지막 for문일 경우 그대로 return */
+                    if(i == location){
+                        answer++;
+                        break out;
+                    }
+                    priorityQueue.poll();
+                    answer++;
+                }
+            }
+        }
+        if(priorityQueue.isEmpty()){
+            answer = -1;
         }
         return answer;
     }
