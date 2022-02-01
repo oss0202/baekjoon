29ab7202d31f63d0371aea3d83ae144beb042704
오성식 <oss0202@naver.com>
2022-02-01 16:09:24 +0900

    프로그래머스 - K번째수

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,90 +1,27 @@
 package programmers;
 
+
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         /**
-         * 1. int 배열을 integer 리스트로 변환
-         * 2. 두개씩 더했을 때 큰수가 되도록 내림차순 정렬
-         * 3. 정렬된 숫자 합치기
-         *  - 첫자리가 0이라면 0
+         * 1. 배열 자르기
+         * 2. 배열 정렬하기
+         * 3. n번째 숫자 뽑아내기
          */
-//        int[] numbers = {6, 10, 2};
-        int[] numbers = {10, 6, 2};
-//        System.out.println(Arrays.toString(numbers));
-        /**
-         * 2개씩 더했을 때 가장 큰 수가 되도록 내림차순 정렬
-         */
-        // int[] -> int stream -> integer stream -> string stream -> String[]
-        String[] strings = Arrays.stream(numbers).boxed().map(String::valueOf).toArray(String[]::new);
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
 
-        System.out.println(Arrays.toString(strings));
+        int[] answer = new int[commands.length];
+        int index = 0;
+        for (int i = 0; i < commands.length; i++) {
+            int[] command = commands[i];
+            int[] result = Arrays.copyOfRange(array, command[0]-1, command[1]);
 
-        String result = Arrays.stream(strings).reduce((s, s2) -> s+=s2).get();
-        System.out.println(result);
-
-/**
-        // 첫번째와 두번째 비교
-        String str1 = strings[0] + strings[1];
-        String str2 = strings[1] + strings[0];
-
-        int int1 = Integer.parseInt(str1);
-        int int2 = Integer.parseInt(str2);
-
-        System.out.println("int1 = " + int1 + ", int2 = " + int2);
-
-        String temp = "";
-        // 둘중 큰 숫자인 것을 찾아서 순서 바꿔주기
-        // 뒤에꺼가 크다면 순서 바꾸기
-        // 앞에꺼가 크다면 기존 유지
-
-        if(int1 < int2){
-            temp = strings[0];
-            strings[0] = strings[1];
-            strings[1] = temp;
+            Arrays.sort(result);
+            answer[index++] = result[command[2]-1];
         }
-        System.out.println(Arrays.toString(strings));
-
-        // 첫번째와 세번째 비교
-        str1 = strings[1] + strings[2];
-        str2 = strings[2] + strings[1];
-        int1 = Integer.parseInt(str1);
-        int2 = Integer.parseInt(str2);
-
-        if(int1 < int2){
-            temp = strings[1];
-            strings[1] = strings[2];
-            strings[2] = temp;
-        }
-        System.out.println(Arrays.toString(strings));
-*/
-    }
-    public String solution(int[] numbers) {
-        String[] strings = Arrays.stream(numbers).boxed().map(String::valueOf).toArray(String[]::new);
-        /**
-         * 배열 돌면서 현재, 다음 원소의 숫자비교
-         */
-        for (int i = 0; i < strings.length - 1; i++) {
-            for (int j = i + 1; j < strings.length; j++) {
-                String temp = "";
-                String string1 = strings[i];
-                String string2 = strings[j];
-
-                String str1 = strings[i] + strings[j];
-                String str2 = strings[j] + strings[i];
-
-                int int1 = Integer.parseInt(str1);
-                int int2 = Integer.parseInt(str2);
-
-                if(int1 < int2){
-                    temp = strings[i];
-                    strings[i] = strings[j];
-                    strings[j] = temp;
-                }
-            }
-        }
-
-        return Arrays.stream(strings).reduce((s, s2) -> s+=s2).get();
+        System.out.println(Arrays.toString(answer));
     }
 }
