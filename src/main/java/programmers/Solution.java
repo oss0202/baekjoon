3aa181499db258137cd235fb0db95f3a88f8220a
=오성식 <oss0202@naver.com>
2022-02-12 17:01:47 +0900

    프로그래머스 > 스택/큐 > 기능개발

--- src/main/java/programmers/Solution.java
+++ src/main/java/programmers/Solution.java
@@ -1,46 +1,30 @@
 package programmers;
 
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 public class Solution {
-    public int solution(int[][] jobs) {
+    public static void main(String[] args) {
+        /**
+         *  parameter : 기능진척도(progresses)배열, 속도(speeds)배열
+         *  return : 각 배포별 배포되는 기능의 수
+         *
+         * 1. 기능과 속도배열에서 한개씩 인자 꺼내기
+         * 2. 배포가능한 일자 구하기
+         * - (100 - 기능진척도) / 속도
+         * 3.
+         * 4.
+        */
+//        int result = (100 - 93) / 1;
 
-        int answer = 0;
-        int end = 0; // 수행되고난 직후의 시간
-        int jobsIdx = 0; // jobs 배열의 인덱스
-        int count = 0; // 수행된 요청 갯수
+        int maxProgress = 100;
+        int nowProgress = 30;
+        int nowSpeed = 30;
+        int mok = (maxProgress - nowProgress) / nowSpeed;
+        int rest = (maxProgress - nowProgress) % nowSpeed;
+        int result = 0;
 
-        // 원본 배열 오름차순 정렬 (요청시간 오름차순)
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-
-        // 처리 시간 오름차순으로 정렬되는 우선순위 큐(Heap)
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-
-        // 요청이 모두 수행될 때까지 반복
-        while (count < jobs.length) {
-
-            // 하나의 작업이 완료되는 시점(end)까지 들어온 모든 요청을 큐에 넣음
-            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= end) {
-                pq.add(jobs[jobsIdx++]);
-            }
-
-            // 큐가 비어있다면 작업 완료(end) 이후에 다시 요청이 들어온다는 의미
-            // (end를 요청의 가장 처음으로 맞춰줌)
-            if (pq.isEmpty()) {
-                end = jobs[jobsIdx][0];
-
-                // 작업이 끝나기 전(end 이전) 들어온 요청 중 가장 수행시간이 짧은 요청부터 수행
-            } else {
-
-                int[] temp = pq.poll();
-                answer += temp[1] + end - temp[0];
-                end += temp[1];
-                count++;
-            }
+        if(rest != 0){
+            result = mok + 1;
         }
-
-        return (int) Math.floor(answer / jobs.length);
+        System.out.println(result);
     }
 }
